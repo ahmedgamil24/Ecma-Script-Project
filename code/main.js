dba262b1bbdce8129ceb706417d6395c81d8e931
ahmedgamil24 <agemail5050@gmail.com>
2025-01-28 12:30:43 +0200

    last updated

--- code/main.js
+++
@@ -1,6 +0,0 @@
-import {add} from "./functions.js"
-
-console.log(add(4,5))
-
-
-console.log("ssssss")
---
+++ code/readytostart.js
@@ -0,0 +1,6 @@
+let btnReady = document.querySelector(".btnReady");
+// console.log(btnReady)
+
+btnReady.addEventListener("click", function(){
+    window.location.href = ""
+})
